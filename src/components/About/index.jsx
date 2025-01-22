4022d41ee7f91c9240b7b742499eb360d3bd4d71
gabrielalencs <gabrieldealencar.dev@gmail.com>
2025-01-21 23:46:55 -0300

    academic training section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,3 +1,7 @@
+// React
+
+import { useState } from "react";
+
 // Styles
 
 import "@/styles/layout/about.scss";
@@ -9,9 +13,12 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa6";
 
 
 const About = () => {
+
+    const [toggleAboutSections, setToggleAboutSections] = useState("biography");
 
     const socialMedias = [
         {
@@ -34,7 +41,11 @@ const About = () => {
             icon: MdEmail,
             link: "sdfsd"
         },
-    ]
+    ];
+
+    const toggleSections = (section) => {
+        setToggleAboutSections(section);
+    };
 
     return (
         <section className="about">
@@ -44,14 +55,14 @@ const About = () => {
 
             <div className="about__container-texts">
                 <div className="about__buttons-infos">
-                    <button className="fancy">
+                    <button className="fancy" onClick={() => toggleSections("biography")}>
                         <span className="top-key"></span>
                         <span className="text">Biografia</span>
                         <span className="bottom-key-1"></span>
                         <span className="bottom-key-2"></span>
                     </button>
 
-                    <button className="fancy">
+                    <button className="fancy" onClick={() => toggleSections("graduation")}>
                         <span className="top-key"></span>
                         <span className="text">Formação</span>
                         <span className="bottom-key-1"></span>
@@ -60,20 +71,112 @@ const About = () => {
                 </div>
 
                 <div className="about__texts">
-                    <h3>Me chamo <br /> <span>Gabriel de Alencar</span></h3>
-                    <p>Sou um Desenvolvedor Frontend, apaixonado por tecnologia e programação. Atualmente, estou no meu último ano Análise e Desenvolvimento de Sistemas. Estou sempre em busca de novos desafios e oportunidades para aprimorar minhas habilidades e conhecimentos.</p>
+                    {toggleAboutSections == "biography" ? (
+                        <>
+                            <h3 className="about__name-title">Me chamo <br /> <span>Gabriel de Alencar</span></h3>
+                            <p className="about__description">Sou um Desenvolvedor Frontend, apaixonado por tecnologia e programação. Atualmente, estou no meu último ano Análise e Desenvolvimento de Sistemas. Estou sempre em busca de novos desafios e oportunidades para aprimorar minhas habilidades e conhecimentos.</p>
 
-                    <div className="about__social-media">
-                        {socialMedias.map(media => (
-                            <a key={media.id} href={media.link}>
-                                <media.icon />
-                            </a>
-                        ))}
-                    </div>
+                            <div className="about__social-media">
+                                {socialMedias.map(media => (
+                                    <a key={media.id} href={media.link}>
+                                        <media.icon />
+                                    </a>
+                                ))}
+                            </div></>
+                    ) : (
+                        <div className="about__graduation-container">
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>02/2025 - 02/2025</span>
+                                    <p>Next do Zero ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+
+                            <div className="about__graduation-item">
+                                <div className="about__graduation-icon">
+                                    <FaGraduationCap />
+                                </div>
+
+                                <div className="about__graduation-infos">
+                                    <span>10/2024 - 11/2025</span>
+                                    <p>TypeScript do básico ao Avançado - Matheus Battisti</p>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </section>
     )
 }
 
-export default About
+export default About;
